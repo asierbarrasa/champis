1cdc64af11803d3e74185168869adba66c4b716c
asierbarrasa <asierjarrillero@gmail.com>
2018-11-09 15:17:10 +0100

    Code beautify

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1; 
-var yyyy = today.getFullYear();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
 
-$(".day").text(dd);
-$(".month").text(mm);
-$(".year").text(yyyy);
-    $(".blk").counterUp({delay:10,time:800});
+    $(".day").text(dd);
+    $(".month").text(mm);
+    $(".year").text(yyyy);
+    $(".blk").counterUp({
+        delay: 10,
+        time: 800
+    });
 });
-
