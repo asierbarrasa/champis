4bc01f215ea4f2c63d1e6ecbfd3a2b13a5f6956b
Asier <asierjarrillero@gmail.com>
2018-11-11 11:35:06 +0100

    responsive and comment added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+var today = new Date();
+
+// Add a given number of 0 at the left part of the number
+function pad_with_zeroes(number, length) {
+
+    var my_string = '' + number;
+    while (my_string.length < length) {
+        my_string = '0' + my_string;
+    }
+
+    return my_string;
+
+}
+
+// counter up to the current date
 $(document).ready(function () {
     var dd = today.getDate();
     var mm = today.getMonth() + 1;
@@ -12,24 +27,16 @@ $(document).ready(function () {
     });
 });
 
-
-var today = new Date();
-
-
-
-
-
-setInterval(function() {
+// change the value of the day, every given time
+setInterval(function () {
 
     today.setDate(today.getDate() + 1);
 
-    var dd = pad_with_zeroes(today.getDate(),2);
-    var mm = pad_with_zeroes(today.getMonth() + 1 ,2);
+    var dd = pad_with_zeroes(today.getDate(), 2);
+    var mm = pad_with_zeroes(today.getMonth() + 1, 2);
     var yyyy = today.getFullYear();
 
     $(".day").text(dd);
     $(".month").text(mm);
     $(".year").text(yyyy);
-
-    
-    }, 900); 
+}, 900);
